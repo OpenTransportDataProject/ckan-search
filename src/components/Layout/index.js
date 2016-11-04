07471f9dbc0bc44e884c62d5829dd3069b09f616
Kristoffer K Larsen <kristoffer@larsen.so>
2016-11-04 18:59:26 +0100

    Initial commit :rocket:

--- src/components/Layout/index.js
+++
@@ -1,3 +0,0 @@
-export Hero from './Hero';
-export Container from './Container';
-export HeroFooter from './HeroFooter';
--- src/store/index.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./production'); // eslint-disable-line global-require
-} else {
-  module.exports = require('./development'); // eslint-disable-line global-require
-}
